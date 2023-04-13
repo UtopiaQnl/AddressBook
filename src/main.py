c33e6265dc8ed79338bd86c8684e0227d27b92f9
Ivan Covach <eeiprex@gmail.com>
2023-04-13 21:33:01 +0300

    [chore] update main.py
    Delete some dunder vars.
    
    Add a new function for check valid path.
    
    Add type hint for controller var.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-#!/c/pythons/Python310/python.exe
 """
 Таск проекта из книги "Byte of Python"
 
@@ -11,37 +10,33 @@ data: 24_12_2022
 
 Python3.10+
 """
-
-__author__ = 'Qu1nel'
-__version__ = 1.0
-
-
 import os
 from pathlib import Path
 from typing import NoReturn, Type
 
 import dill
 
+from core import Core
 from core.Controller import Controller
 
-from .supportive.State import State
-from core import Core
+from supportive.State import State
 from db.book_table import BookTable
 
 from config import *
 
 
+def path_exists(path: Path | str) -> bool:
+    return (isinstance(path, Path) and path.exists()) or os.path.exists(path)
+
+
 def read_address_book_from_local_file(path_to_database: Path | str) -> BookTable:
-    """Читает базу данных контактов из файла если та существует, иначе создает новую."""
-
-    if (isinstance(path_to_database, Path) and path_to_database.exists()) or \
-            os.path.exists(path_to_database):
+    if path_exists(path_to_database):
         with open(path_to_database, mode='rb') as file_database:
-            _book = dill.load(file_database)
+            book = dill.load(file_database)
     else:
-        _book: BookTable = BookTable()
+        book: BookTable = BookTable()
 
-    return _book
+    return book
 
 
 def main() -> NoReturn:
@@ -49,7 +44,7 @@ def main() -> NoReturn:
 
     db: BookTable = read_address_book_from_local_file(SAVE_DB_PATH)
     while True:
-        controller = Core.get_controller_by_state(state_program)
+        controller: Type[Controller] = Core.get_controller_by_state(state_program)
         controller_entity = controller(state=state_program, data=db)
         state_program = controller_entity.run()
 
